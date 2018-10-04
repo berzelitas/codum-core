a9353d7cc9163c9366be99b592856f0e9ac318b9
Dmitry Ruban <dkruban@gmail.com>
2018-10-04 20:47:06 +0300

    CODUM-24 - addmilestone action in mbps. Small code refactoring.

--- contracts/mbps/mbps.cpp
+++ contracts/mbps/mbps.cpp
@@ -56,7 +56,7 @@ void bond::issue(account_name to, asset quantity, string memo) {
 
 void bond::distrtokens(id_type token_id) {
     // token bondtoken = tokens.find(token_id);
-    // stat bs = 
+    // stat bs =
 }
 
 void bond::transfer(account_name from, account_name to, asset quantity, string memo) {
@@ -79,98 +79,146 @@ void bond::transfer(account_name from, account_name to, asset quantity, string m
     add_balance(to, quantity, from);
 }
 
-void bond::transferid( account_name from, account_name to, id_type id, string memo ) {
+void bond::transferid(account_name from, account_name to, id_type id, string memo) {
     // Ensure authorized to send from account
-    eosio_assert( from != to, "cannot transfer to self" );
-    require_auth( from );
+    eosio_assert(from != to, "cannot transfer to self");
+    require_auth(from);
 
     // Ensure 'to' account exists
-    eosio_assert( is_account( to ), "to account does not exist");
+    eosio_assert(is_account(to), "to account does not exist");
 
-	// Check memo size and print
-    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+    // Check memo size and print
+    eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
 
     // Ensure token ID exists
-    auto sender_token = tokens.find( id );
-    eosio_assert( sender_token != tokens.end(), "token with specified ID does not exist" );
+    auto sender_token = tokens.find(id);
+    eosio_assert(sender_token != tokens.end(), "token with specified ID does not exist");
 
-	// Ensure owner owns token
-    eosio_assert( sender_token->owner == from, "sender does not own token with specified ID");
+    // Ensure owner owns token
+    eosio_assert(sender_token->owner == from, "sender does not own token with specified ID");
 
-	const auto& st = *sender_token;
+    const auto &st = *sender_token;
 
-	// Notify both recipients
-    require_recipient( from );
-    require_recipient( to );
+    // Notify both recipients
+    require_recipient(from);
+    require_recipient(to);
 
     // Transfer NFT from sender to receiver
-    tokens.modify( st, from, [&]( auto& token ) {
-	    token.owner = to;
+    tokens.modify(st, from, [&](auto &token) {
+        token.owner = to;
     });
 
     // Change balance of both accounts
-    sub_balance( from, st.value );
-    add_balance( to, st.value, from );
+    sub_balance(from, st.value);
+    add_balance(to, st.value, from);
 }
 
-void bond::burn( account_name owner, id_type token_id )
-{
-    require_auth( owner );
+void bond::burn(account_name owner, id_type token_id) {
+    require_auth(owner);
 
     // Find token to burn
-    auto burn_token = tokens.find( token_id );
-	eosio_assert( burn_token != tokens.end(), "token with id does not exist" );
-	eosio_assert( burn_token->owner == owner, "token not owned by account" );
+    auto burn_token = tokens.find(token_id);
+    eosio_assert(burn_token != tokens.end(), "token with id does not exist");
+    eosio_assert(burn_token->owner == owner, "token not owned by account");
 
-	asset burnt_supply = burn_token->value;
+    asset burnt_supply = burn_token->value;
 
-	// Remove token from tokens table
-    tokens.erase( burn_token );
+    // Remove token from tokens table
+    tokens.erase(burn_token);
 
     // Lower balance from owner
-    sub_balance( owner, burnt_supply );
+    sub_balance(owner, burnt_supply);
 
     // Lower supply from currency
-    sub_supply( burnt_supply );
+    sub_supply(burnt_supply);
 }
 
 void bond::setrampayer(account_name payer, id_type id) {
-	require_auth(payer);
+    require_auth(payer);
 
-	// Ensure token ID exists
-	auto payer_token = tokens.find( id );
-	eosio_assert( payer_token != tokens.end(), "token with specified ID does not exist" );
+    // Ensure token ID exists
+    auto payer_token = tokens.find(id);
+    eosio_assert(payer_token != tokens.end(), "token with specified ID does not exist");
 
-	// Ensure payer owns token
-	eosio_assert( payer_token->owner == payer, "payer does not own token with specified ID");
+    // Ensure payer owns token
+    eosio_assert(payer_token->owner == payer, "payer does not own token with specified ID");
 
-	const auto& st = *payer_token;
+    const auto &st = *payer_token;
 
-	// Notify payer
-	require_recipient( payer );
+    // Notify payer
+    require_recipient(payer);
 
-	// Set owner as a RAM payer
-	tokens.modify(payer_token, payer, [&](auto& token){
-		token.id = st.id;
-		token.uri = st.uri;
-		token.owner = st.owner;
-		token.value = st.value;
-		token.name = st.name;
-	});
+    // Set owner as a RAM payer
+    tokens.modify(payer_token, payer, [&](auto &token) {
+        token.id = st.id;
+        token.uri = st.uri;
+        token.owner = st.owner;
+        token.value = st.value;
+        token.name = st.name;
+    });
 
-	sub_balance( payer, st.value );
-	add_balance( payer, st.value, payer );
+    sub_balance(payer, st.value);
+    add_balance(payer, st.value, payer);
 }
 
-void bond::mint( account_name owner, account_name ram_payer, asset value, string uri, string name)
-{
+void bond::mint(account_name owner, account_name ram_payer, asset value, string uri, string name) {
     // Add token with creator paying for RAM
-    tokens.emplace( ram_payer, [&]( auto& token ) {
+    tokens.emplace(ram_payer, [&](auto &token) {
         token.id = tokens.available_primary_key();
         token.uri = uri;
         token.owner = owner;
         token.value = value;
-	    token.name = name;
+        token.name = name;
+    });
+}
+
+void bond::addmilestone(
+  asset bond,
+  uint16_t weight,
+  time deadline,
+  asset budget,
+  asset token_budget,
+  string milestone,
+  string press_release_url,
+  string epic_url
+) {
+    auto bond_symbol = bond.symbol;
+    eosio_assert(bond_symbol.is_valid(), "Invalid symbol name.");
+    eosio_assert(budget.symbol.is_valid(), "Invalid symbol name.");
+    eosio_assert(token_budget.symbol.is_valid(), "Invalid symbol name.");
+
+    stats statstable(_self, bond_symbol.name());
+    auto bs = statstable.find(bond_symbol.name());
+
+    eosio_assert(bs != statstable.end, "Non-existed stats for bond.");
+    require_auth(bs.token_contract);
+
+    eosio_assert(deadline > bs.start && deadline < bs.end, "The deadline must be between of start and end.");
+
+    eosio_assert(strlen(milestone) >= 16, "The milestone length must be at least 16 characters.");
+
+    eosio_assert(bond.quantity == 0, "Bond quantity must be equal to 0.");
+
+    eosio_assert(weight <= 1000000, "Total weight of all bond.symbol related milestones must not exceed 1000000.");
+
+    accounts from_budget_acnts(bs.budget_contract, _self);
+    const auto &budget_acc = from_budget_acnts.get(budget.symbol.name());
+    eosio_assert(budget_acc.balance >= 0, "Budget must be a valid asset symbol with 0 or positive quantity.");
+
+    accounts from_token_acnts(bs.token_contract, _self);
+    const auto &token_acc = from_token_acnts.get(token_budget.symbol.name());
+    eosio_assert(token_acc.balance >= 0, "Token budget must be a valid asset symbol with 0 or positive quantity.");
+
+    schedules schedulable(_self, bond.symbol.name());
+    schedulable.emplace(_self, [&](auto &s) {
+        s.id = 0;
+        s.bond = bond;
+        s.weight = weight;
+        s.deadline = deadline;
+        s.budget = budget;
+        s.token_budget = token_budget;
+        s.funding_status = 0;
+        s.execution_status = 0;
     });
 }
 
@@ -202,25 +250,25 @@ void bond::add_balance(account_name owner, asset value, account_name ram_payer) 
     }
 }
 
-void bond::sub_supply( asset quantity ) {
+void bond::sub_supply(asset quantity) {
     auto symbol_name = quantity.symbol.name();
-    stats currency_table( _self, symbol_name );
-    auto current_currency = currency_table.find( symbol_name );
+    stats currency_table(_self, symbol_name);
+    auto current_currency = currency_table.find(symbol_name);
 
-    currency_table.modify( current_currency, 0, [&]( auto& currency ) {
+    currency_table.modify(current_currency, 0, [&](auto &currency) {
         currency.supply -= quantity;
     });
 }
 
-void bond::add_supply( asset quantity ) {
+void bond::add_supply(asset quantity) {
     auto symbol_name = quantity.symbol.name();
-    stats currency_table( _self, symbol_name );
-    auto current_currency = currency_table.find( symbol_name );
+    stats currency_table(_self, symbol_name);
+    auto current_currency = currency_table.find(symbol_name);
 
-    currency_table.modify( current_currency, 0, [&]( auto& currency ) {
+    currency_table.modify(current_currency, 0, [&](auto &currency) {
         currency.supply += quantity;
     });
 }
 }; // namespace eosio
 
-EOSIO_ABI(eosio::bond, (create)(issue)(distrtokens)(transfer)(transferid)(burn)(setrampayer));
+EOSIO_ABI(eosio::bond, (create)(issue)(distrtokens)(transfer)(transferid)(burn)(setrampayer)(addmilestone));
--- contracts/mbps/mbps.hpp
+++ contracts/mbps/mbps.hpp
@@ -10,149 +10,170 @@ class system_contract;
 }
 
 namespace eosio {
-  using std::string;
-  typedef uint128_t uuid;
-  typedef uint64_t id_type;
-  typedef string uri_type;
+using std::string;
+typedef uint128_t uuid;
+typedef uint64_t id_type;
+typedef string uri_type;
 
-  class bond : public contract {
-  public:
-    bond(account_name self) : contract(self), tokens(_self, _self) {}
+class bond : public contract {
+public:
+  bond(account_name self) : contract(self), tokens(_self, _self) {}
 
-    [[eosio::action]]
-    void create(account_name issuer, asset maximum_supply);
+  [[eosio::action]]
+  void create(account_name issuer, asset maximum_supply);
 
-    [[eosio::action]]
-    void issue(account_name to, asset quantity, string memo);
+  [[eosio::action]]
+  void issue(account_name to, asset quantity, string memo);
 
-    [[eosio::action]]
-    void distrtokens(id_type token_id);
+  [[eosio::action]]
+  void distrtokens(id_type token_id);
 
-    [[eosio::action]]
-    void transfer(account_name from, account_name to, asset quantity, string memo);
+  [[eosio::action]]
+  void transfer(account_name from, account_name to, asset quantity, string memo);
 
-    [[eosio::action]]
-    void transferid(account_name from, account_name to, id_type id, string memo);
+  [[eosio::action]]
+  void transferid(account_name from, account_name to, id_type id, string memo);
 
-    [[eosio::action]]
-    void burn(account_name owner, id_type token_id);
+  [[eosio::action]]
+  void burn(account_name owner, id_type token_id);
 
-    [[eosio::action]]
-    void setrampayer(account_name payer, id_type id);
-    
+  [[eosio::action]]
+  void setrampayer(account_name payer, id_type id);
 
-    struct [[eosio::table]] account {
-      asset balance;
+  [[eosio::action]]
+  void addmilestone(
+    asset bond,
+    uint16_t weight,
+    time deadline,
+    asset budget,
+    asset token_budget,
+    string milestone,
+    string press_release_url,
+    string epic_url
+  );
 
-      uint64_t primary_key() const { return balance.symbol.name(); }
-    };
+  struct [[eosio::table]] account {
+    asset balance;
 
-    struct [[eosio::table]] stat {
-      asset supply;
-      asset max_supply;
-      account_name issuer;
-      string project;
+    uint64_t primary_key() const { return balance.symbol.name(); }
+  };
 
-      uint64_t primary_key() const { return supply.symbol.name(); }
-    };
+  struct [[eosio::table]] stat {
+    asset supply;
+    asset max_supply;
+    account_name issuer;
+    string project;
 
-    struct [[eosio::table]] token {
-      id_type id;          // Unique 64 bit identifier
-      uri_type uri;        // RFC 3986
-      account_name owner;  // token owner
-      asset value;         // token value (1 SYS)
-      string name;	 // token name
+    uint64_t primary_key() const { return supply.symbol.name(); }
+  };
 
-      id_type primary_key() const { return id; }
-      account_name get_owner() const { return owner; }
-      string get_uri() const { return uri; }
-      asset get_value() const { return value; }
-      uint64_t get_symbol() const { return value.symbol.name(); }
-      uint64_t get_name() const { return string_to_name(name.c_str()); }
+  struct [[eosio::table]] token {
+    id_type id;          // Unique 64 bit identifier
+    uri_type uri;        // RFC 3986
+    account_name owner;  // token owner
+    asset value;         // token value (1 SYS)
+    string name;     // token name
 
-      uuid get_global_id() const {
+    id_type primary_key() const { return id; }
+
+    account_name get_owner() const { return owner; }
+
+    string get_uri() const { return uri; }
+
+    asset get_value() const { return value; }
+
+    uint64_t get_symbol() const { return value.symbol.name(); }
+
+    uint64_t get_name() const { return string_to_name(name.c_str()); }
+
+    uuid get_global_id() const {
         uint128_t self_128 = static_cast<uint128_t>(N(_self));
         uint128_t id_128 = static_cast<uint128_t>(id);
         uint128_t res = (self_128 << 64) | (id_128);
         return res;
-      }
+    }
 
-      string get_unique_name() const {
+    string get_unique_name() const {
         string unique_name = name + "#" + std::to_string(id);
         return unique_name;
-      }
-    };
+    }
+  };
 
-    struct [[eosio::table]] schedule {
-      id_type id;
-      asset bond;
-      uint8_t weight;
-      time deadline;
-      asset budget;
-      asset token_budget;
-      uint8_t funding_status;
-      uint8_t execution_status;
+  struct [[eosio::table]] schedule {
+    id_type id;
+    asset bond;
+    uint8_t weight;
+    time deadline;
+    asset budget;
+    asset token_budget;
+    uint8_t funding_status;
+    uint8_t execution_status;
 
-      uint64_t primary_key() const { return id; }
+    uint64_t primary_key() const { return id; }
 
-      uint64_t get_bond_symbol() const { return bond.symbol.name(); }
-    };
+    uint64_t get_bond_symbol() const { return bond.symbol.name(); }
+  };
 
-    struct [[eosio::table]] milestoneclaims {
+  struct [[eosio::table]] milestoneclaims {
     id_type id;
     id_type token_id;
     id_type milestone_id;
 
     uint64_t primary_key() const { return id; };
+
     uint64_t get_token_id() const { return token_id; };
+
     uint64_t get_milestone_id() const { return milestone_id; };
   };
 
-    using account_index = eosio::multi_index<N(accounts), account>;
+  using account_index = eosio::multi_index<N(accounts), account>;
 
-    using stats = eosio::multi_index<N(stat), stat>;
+  using stats = eosio::multi_index<N(stat), stat>;
 
-    using token_index = eosio::multi_index<N(token), token,
-                                            indexed_by< N( byowner ), const_mem_fun< token, account_name, &token::get_owner> >,
-                                            indexed_by< N( bysymbol ), const_mem_fun< token, uint64_t, &token::get_symbol> >,
-                                            indexed_by< N( byname ), const_mem_fun< token, uint64_t, &token::get_name> > >;
+  using token_index = eosio::multi_index<N(token), token,
+    indexed_by< N(byowner), const_mem_fun < token, account_name, &token::get_owner> >,
+    indexed_by< N(bysymbol), const_mem_fun < token, uint64_t, &token::get_symbol> >,
+    indexed_by< N(byname), const_mem_fun < token, uint64_t, &token::get_name> > >;
 
-    using schedules = eosio::multi_index<N(schedule), schedule,
-                                          indexed_by < N(bond_symbol), const_mem_fun < schedule, uint64_t, &schedule::get_bond_symbol> > >;
+  using schedules = eosio::multi_index<N(schedule), schedule,
+    indexed_by< N(bond_symbol), const_mem_fun < schedule, uint64_t, &schedule::get_bond_symbol> > >;
 
-    using milestoneclaims_index = eosio::multi_index<N(milestone), milestoneclaims, 
-                                                    indexed_by<N(bytoken), eosio::const_mem_fun< milestoneclaims, uint64_t, &milestoneclaims::get_token_id> >,
-                                                    indexed_by<N(bymilestone), eosio::const_mem_fun< milestoneclaims, uint64_t, &milestoneclaims::get_milestone_id> > >;
+  using milestoneclaims_index = eosio::multi_index<N(milestone), milestoneclaims,
+    indexed_by< N(bytoken), eosio::const_mem_fun<milestoneclaims, uint64_t, &milestoneclaims::get_token_id> >,
+    indexed_by< N(bymilestone), eosio::const_mem_fun<milestoneclaims, uint64_t, &milestoneclaims::get_milestone_id> > >;
 
 
-  private:
-    friend eosiosystem::system_contract;
+private:
+  friend eosiosystem::system_contract;
 
-    token_index tokens;
+  token_index tokens;
 
-    // PRIVATE UTILITY FUNCTIONS
-    void mint(account_name owner, account_name ram_payer, asset value, string uri, string name);
-    
-    void sub_balance(account_name owner, asset value);
-    void add_balance(account_name owner, asset value, account_name ram_payer);
-    void sub_supply(asset quantity);
-    void add_supply(asset quantity);
+  // PRIVATE UTILITY FUNCTIONS
+  void mint(account_name owner, account_name ram_payer, asset value, string uri, string name);
 
-    inline asset get_supply(symbol_name sym) const;
+  void sub_balance(account_name owner, asset value);
 
-    inline asset get_balance(account_name owner, symbol_name sym) const;
-  };
+  void add_balance(account_name owner, asset value, account_name ram_payer);
 
-  asset bond::get_supply(symbol_name sym) const {
-      stats statstable(_self, sym);
-      const auto &st = statstable.get(sym);
-      return st.supply;
-  }
+  void sub_supply(asset quantity);
 
-  asset bond::get_balance(account_name owner, symbol_name sym) const {
-      account_index accountstable(_self, owner);
-      const auto &ac = accountstable.get(sym);
-      return ac.balance;
-  }
+  void add_supply(asset quantity);
+
+  inline asset get_supply(symbol_name sym) const;
+
+  inline asset get_balance(account_name owner, symbol_name sym) const;
+};
+
+asset bond::get_supply(symbol_name sym) const {
+    stats statstable(_self, sym);
+    const auto &st = statstable.get(sym);
+    return st.supply;
+}
+
+asset bond::get_balance(account_name owner, symbol_name sym) const {
+    account_index accountstable(_self, owner);
+    const auto &ac = accountstable.get(sym);
+    return ac.balance;
+}
 
 }; // namespace eosio
